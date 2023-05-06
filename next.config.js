55a5b24053f0050ad4b2db252cd2286a4238f7ab
navanshu upadhyay <64829326+senator23@users.noreply.github.com>
2023-05-06 12:34:26 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+// module.exports = {
+//   reactStrictMode: true,
+// }
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['rb.gy'],
+  },
+}
+
+
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+// const printhello = name => console.log('Arrow function',name)
+
+// if(true) printhello("nanu ")
+
+function about() {
+  return (
+    <div>Water</div>
+  )
+}
+
+export default about
